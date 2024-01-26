baa9cafae80b0743bf00fc35cd58785f76547186
Vĩnh Phước <vinhphuoc9919@gmail.com>
2024-01-26 07:05:57 +0700

    implement url repo

---
+++ migrations/24012606.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE
+    IF NOT EXISTED "url_lookup" (
+        "hash" varchar(24) NOT NULL,
+        "url" text NOT NULL CONSTRAINT "uq__url_look_up__url" UNIQUE,
+        "created_at" timestamptz NOT NULL DEFAULT current_timestamp,
+        PRIMARY KEY ("hash")
+    );
