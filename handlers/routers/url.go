4d8dd9f41b1772a4ab36df0105487e7ecfc42200
Vĩnh Phước <vinhphuoc9919@gmail.com>
2024-01-28 21:02:53 +0700

    add htmx

--- handlers/routers/url.go
+++ handlers/routers/url.go
@@ -25,27 +25,43 @@ func (h *URL) Route(serveMux *http.ServeMux) {
 	serveMux.HandleFunc("/", h.handle)
 }
 
-func (h *URL) handle(w http.ResponseWriter, res *http.Request) {
-	uri := res.RequestURI
+func (h *URL) handle(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		h.handleGet(w, req)
+	case http.MethodPost:
+		h.handlePost(w, req)
+	default:
+		w.Header().Add("Allow", fmt.Sprintf("%v, %v", http.MethodGet, http.MethodPost))
+		http.Error(w, fmt.Sprintf("%v method is not allowed", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (h *URL) handlePost(w http.ResponseWriter, req *http.Request) {
+}
+
+func (h *URL) handleGet(w http.ResponseWriter, req *http.Request) {
+	uri := req.RequestURI
 	if uri[0] == '/' {
 		uri = uri[1:]
 	}
 	if len(uri) == 0 {
-		h.showIndexPage(w, res)
+		h.showIndexPage(w, req)
 		return
 	}
 
 	_, err := url.ParseRequestURI(uri)
 	if err != nil {
-		h.getFullURL(w, res, uri)
+		h.getFullURL(w, req, uri)
 	} else {
-		h.getHash(w, res, uri)
+		h.getHash(w, req, uri)
 	}
 }
 
-func (h *URL) getHash(w http.ResponseWriter, res *http.Request, url string) {
+func (h *URL) getHash(w http.ResponseWriter, req *http.Request, url string) {
 	url = h.fix(url)
-	hash, err := h.service.GetHash(res.Context(), url)
+	hash, err := h.service.GetHash(req.Context(), url)
 	if err != nil {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -66,8 +82,8 @@ func (h *URL) fix(url string) string {
 	)
 }
 
-func (h *URL) getFullURL(w http.ResponseWriter, res *http.Request, hash string) {
-	url, err := h.service.GetFullURL(res.Context(), hash)
+func (h *URL) getFullURL(w http.ResponseWriter, req *http.Request, hash string) {
+	url, err := h.service.GetFullURL(req.Context(), hash)
 	if err != nil {
 		if terr, ok := err.(utils.TraceError); ok && terr.Is(sql.ErrNoRows) {
 			http.Error(w, err.Error(), http.StatusNotFound)
@@ -80,9 +96,9 @@ func (h *URL) getFullURL(w http.ResponseWriter, res *http.Request, hash string) 
 	w.WriteHeader(http.StatusMovedPermanently)
 }
 
-func (h *URL) showIndexPage(w http.ResponseWriter, res *http.Request) {
+func (h *URL) showIndexPage(w http.ResponseWriter, req *http.Request) {
 	data := &types.HTMLIndexView{
-		AcceptLanguage: res.Header.Get("Accept-Language"),
+		AcceptLanguage: req.Header.Get("Accept-Language"),
 	}
 
 	if err := h.index.Exec(w, data); err != nil {
